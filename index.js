656c838ceee13db152ca29cb4f4b089f3f377288
veda1612 <vedavasavisiri@gmail.com>
2021-01-14 20:24:12 +0000

    refactored

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Arrays to keep track of each task's state
+/*// Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
 
@@ -28,4 +28,35 @@ newTask("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(0); // Clean Cat Litter has been completed */
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+};
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2]
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);
+
